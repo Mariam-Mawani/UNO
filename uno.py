91fa5f1712b6f7581baacd7b6958bfc5ea739e45
mamawjhb025 <mamawjhb025@student.wethinkcode.co.za>
2026-08-02 14:45:44 +0200

    for loop for card deck added

--- uno.py
+++ uno.py
@@ -32,4 +32,13 @@ def deck_of_cards():
     values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "skip", "reverse", "draw 2"]
     wild_cards = ["wild draw 4", "wild card"]
 
-    
+    for colour in colours:
+        for value in values:
+            cardVal = "{} {}".format(colour, value)
+            deck.append(cardVal)
+            # Two of each card, excluding 0
+            if value != 0: 
+                cardVal = "{} {}".format(colour, value)
+                deck.append(cardVal)
+
+                
